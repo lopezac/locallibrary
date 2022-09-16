5bc98b43b399a7f1db783321be16884c6f77ed22
Axel C. Lopez <lopezaxel@protonmail.com>
2022-09-16 17:12:42 -0400

    Work on form

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var coolRouter = require("./routes/cool");
-const catalogRouter = require("./routes/catalog");
+var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
